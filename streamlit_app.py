16d263982033343809a3bc8aaa5c98fc302d9ef3
IKESUYI <156671923+IKESUYI@users.noreply.github.com>
2025-01-20 12:29:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title('Machine Learning App')
 
 st.info('This App builds machine learning model')
+
+df=pd.read_csv('https://github.com/IKESUYI/ike-machinelearning/blob/master/penguin%20dataset.csv')
+df
 
 
