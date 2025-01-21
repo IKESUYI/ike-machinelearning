6dae4a094f19e2d594ccaa104b37c0cc5869a78a
IKESUYI <156671923+IKESUYI@users.noreply.github.com>
2025-01-21 12:53:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,12 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X=df.drop('species',axis=1)
-  X
+  X_raw=df.drop('species',axis=1)
+  X_raw
 
   st.write('**y**')
-  y=df.species
-  y
+  y_raw=df.species
+  y_raw
 
 with st.expander('Data Visualization'):
   #bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g
@@ -30,7 +30,7 @@ with st.sidebar:
   st.header('Input Features')
   # island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex
   island= st.selectbox('Island', ('Torgersen','Biscoe','Dream'))
-  gender= st.selectbox('Gender', ('male','female'))
+  sex= st.selectbox('Sex', ('male','female'))
   bill_length_mm= st.slider('Bill length (mm)', 32.1,59.6,43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1,21.5,17.2)
   flipper_length_mm= st.slider('Flipper length (mm)',172.0,231.0,201.0)
@@ -42,15 +42,36 @@ with st.sidebar:
         'bill_depth_mm': bill_depth_mm,
         'flipper_length_mm':flipper_length_mm,
         'body_mass_g':body_mass_g,
-        'gender':gender}
+        'sex':sex}
   input_df=pd.DataFrame(data, index=[0])
-  input_penguins= pd.concat([input_df,X], axis=0)
+  input_penguins= pd.concat([input_df,X_raw], axis=0)
+
+# Encode X
+encode= ['island','sex']
+df_penguins= pd.get_dummies(input_penguins,prefix=encode)
+input_row=df_penguins[:1]
+
+#encode y:
+target_mapper={'Adelie':0,
+               'Chinstrap':1,
+               'Gentoo':2}
+def target_encode(val):
+  return target_mapper[val]
+
+y= y_raw.apply(target_encode)
+y
+y_raw
+
 
 with st.expander('Input features'):
   st.write('**Input penguin**')
   input_df
   st.write('**Combined penguins data**')
   input_penguins
+  st.write('**Encoded input penguins**')
+  input_row
+
+
 
  
         
