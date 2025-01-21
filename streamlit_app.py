27138aa805a68c5444b95e59a5fcb3ada75c2280
IKESUYI <156671923+IKESUYI@users.noreply.github.com>
2025-01-21 12:14:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ with st.sidebar:
   #create a Dataframe for the input features
   data={'island':island,
         'bill_length_mm':bill_length_mm,
+        'bill_depth_mm': bill_depth_mm,
         'flipper_length_mm':flipper_length_mm,
         'body_mass_g':body_mass_g,
         'gender':gender}
